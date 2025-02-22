a5141bfdd6b16e9d28b506530029846d4d1eeea7
Gryphon Picciottoli <gryphninja@gmail.com>
2025-02-21 22:06:44 -0500

    Added a griffin script

---
+++ Assets/Scripts/Griffin.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.TextCore.Text;
+using UnityEngine.UIElements;
+
+public class Griffin : Animal, IInteractable, IPet
+{
+    // Refrences the script that takes in the player's input
+    public PlayControlScript controller;
+
+    public override void feed()
+    {
+
+        Debug.Log("Test 3");
+        hunger++;
+        this.textOutput.text = "You fed the griffin. He seems very satisfied! His hunger is " + hunger.ToString();
+
+    }
+
+    public override void petAnimal()
+    {
+        happiness*=2.0f;
+        this.textOutput.text = "You pet the griffin. He seems happy! His happiness is " + happiness.ToString();
+    }
+
+    void Start()
+    {
+
+        // Defines all the properties of the dragon
+        this.hunger = 100.0f;
+        this.maxHunger = 100.0f;
+        this.happiness = 100;
+        this.GameObj = GetComponent<GameObject>();
+
+    }
+
+    void Update()
+    {
+
+        // I'm using distance bc ontriggerenter is not working for some reason and I am running very low on time
+        float Distance = Mathf.Sqrt(Mathf.Pow((Player.transform.position.x - transform.position.x), 2.0f) + Mathf.Pow((Player.transform.position.y - transform.position.y), 2.0f));
+        if (Distance <= 5.0f) {
+            Debug.Log("Enter");
+            controller.SetIInstance(this);
+            controller.SetIPet(this);
+        }
+        else {
+            Debug.Log("Exit");
+            controller.ClearIInstance();
+            controller.ClearIPet();
+        }
+    }
+
+    public void interact()
+    {
+        Debug.Log("Test 4");
+        feed();
+    }
+
+
+    public void pet() {
+        petAnimal();
+    }
+}
+
